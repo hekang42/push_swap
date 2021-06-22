e00ccf8ca95be41030693f18d311d9200f79a794
Hee young Kang <hekang@student.42seoul.kr>
2021-06-22 13:59:01 +0900

    4 / 3

--- srcs/sort.c
+++ srcs/sort.c
@@ -6,7 +6,7 @@
 /*   By: hekang <hekang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/21 16:24:21 by hekang            #+#    #+#             */
-/*   Updated: 2021/06/22 12:40:45 by hekang           ###   ########.fr       */
+/*   Updated: 2021/06/22 13:58:42 by hekang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,14 +66,12 @@ int			find_index_smaller(int find, t_node *stack)
 		rear_count++;
 		cur = cur->prev;
 	}
-	// printf("front_count: %d\n", front_count);
-	// printf("rear_count: %d\n", rear_count);
 	if (front_count >= rear_count)
 		return (ft_stacksize(stack) - rear_count);
 	return (front_count);
 }
 
-void		sort_from_b(t_node **stack_a, t_node **stack_b)
+void		sort_from_b2(t_node **stack_a, t_node **stack_b)
 {
 	int		find_index;
 	int		rb_count;
@@ -101,6 +99,67 @@ void		sort_from_b(t_node **stack_a, t_node **stack_b)
 			while(rrb_count-- + 1)
 				operator("rrb", stack_a, stack_b);
 		operator("pa", stack_a, stack_b);
+		// if (flag == 1)
+		// 	operator("sa", stack_a, stack_b);
+	}
+}
+
+void		sort_from_b(t_node **stack_a, t_node **stack_b)
+{
+	int		find_index;
+	int		rb_count;
+	int		rrb_count;
+	int		flag;
+	// int		size;
+
+	flag = 0;
+	put_index(*stack_b);
+	find_index = ft_stacksize(*stack_b) -  1;
+
+	while (*stack_b)
+	{
+		rb_count = find_from_front(find_index, *stack_b); 
+		rrb_count = find_from_rear(find_index--, *stack_b);
+		// printf("find_index: %d, b_index: %d, b_content: %d\n", find_index + 1, (*stack_b)->index, (*stack_b)->content);
+		// printf("rb_count : %d, rrb_count : %d\n", rb_count, rrb_count);
+		// if (find_from_front(find_index, *stack_b) == 0)
+		// {
+		// 	operator("rb", stack_a, stack_b);
+		// 	rb_count--;
+		// 	flag = 1;
+		// }
+		if (rb_count < rrb_count)
+			while(rb_count-- && (*stack_b)->index != find_index + 1)
+				if ((*stack_b)->index == find_index)
+				{
+					operator("pa", stack_a, stack_b);
+					flag = 1;
+				}
+				else
+					operator("rb", stack_a, stack_b);
+		else
+			while(rrb_count-- + 1 && (*stack_b)->index != find_index + 1)
+				if ((*stack_b)->index == find_index)
+				{
+					operator("pa", stack_a, stack_b);
+					operator("rrb", stack_a, stack_b);
+					flag = 1;
+				}
+				// else if (find_index > 0 && (*stack_b)->index == find_index - 1)
+				// {
+				// 	operator("pa", stack_a, stack_b);
+
+				// }
+				else
+					operator("rrb", stack_a, stack_b);
+		operator("pa", stack_a, stack_b);
+		if (flag == 1)
+		{
+			find_index--;
+			operator("sa", stack_a, stack_b);
+			flag = 0;
+		}
+		// print_stack(*stack_b);
 		// if (flag == 1)
 		// 	operator("sa", stack_a, stack_b);
 	}
